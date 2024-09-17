3d995f8560a37e3b470b330ad7c52dd6cddba3bf
Nurunnahar <nurunnahar9081@gmail.com>
2024-09-17 16:31:47 +0600

    technology design done

---
+++ src/components/Technology/Technology.jsx
@@ -0,0 +1,28 @@
+import './Technology.css';
+
+import React from "react";
+
+const Technology = () => {
+  return (
+    <>
+      <div className="technology-area">
+        <div className="technology">
+          <div className="techno-left-side">
+            <h1>
+              The fastest way to grow your business with the leader in
+              <span>Technology</span>
+            </h1>
+            <p>Check out our options and features included.</p>
+          </div>
+          <div className="techno-right-side">
+            <button>Get Started Now</button>
+            <span>or</span>
+            <a href="#">learn more</a>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Technology;
--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Slider from '../../components/Slider/Slider'
+import Technology from '../../components/Technology/Technology'
 
 const HomePage = () => {
   return (
       <>
-      <Slider/>
+      <Slider />
+      <Technology/>
       </>
   )
 }
