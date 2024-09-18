fb8f1815bc678802985fe50e8fb8f77f85896c47
Nurunnahar <nurunnahar9081@gmail.com>
2024-09-18 13:21:57 +0600

    feature left side  design done

---
+++ src/components/Feature/Feature.jsx
@@ -0,0 +1,108 @@
+import "./Feature.css";
+import React from "react";
+
+const Feature = () => {
+  return (
+    <>
+      <div className="feature-area">
+        <div className="heading">
+          <h2>Our Features</h2>
+        </div>
+        <div className="feature">
+          <div className="item">
+            <div className="single-item">
+              <div className="title">
+                <i class="fa-solid fa-life-ring icon"></i>
+                <h3>Customer Support</h3>
+              </div>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Phasellus blandit massa
+              </p>
+            </div>
+            <div className="single-item">
+              <div className="title">
+                <i class="fa-solid fa-layer-group icon"></i>
+                <h3>Sliders</h3>
+              </div>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Phasellus blandit massa
+              </p>
+            </div>
+            <div className="single-item">
+              <div className="title">
+                <i class="fa-regular fa-file-lines icon"></i>
+                <h3>HTML5 / CSS3 / JS</h3>
+              </div>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Phasellus blandit massa
+              </p>
+            </div>
+            <div className="single-item">
+              <div className="title">
+                <i class="fa-brands fa-google icon"></i>
+                <h3>500+ Google Fonts</h3>
+              </div>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Phasellus blandit massa
+              </p>
+            </div>
+            <div className="single-item">
+              <div className="title">
+                <i class="fa-regular fa-user icon"></i>
+                <h3>Icons</h3>
+              </div>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Phasellus blandit massa
+              </p>
+            </div>
+            <div className="single-item">
+              <div className="title">
+                <i class="fa-solid fa-bars icon"></i>
+                <h3>Buttons</h3>
+              </div>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Phasellus blandit massa
+              </p>
+            </div>
+            <div className="single-item">
+              <div className="title">
+                <i class="fa-solid fa-display icon"></i>
+                <h3>Lightbox</h3>
+              </div>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Phasellus blandit massa
+              </p>
+            </div>
+            <div className="single-item">
+              <div className="title">
+                <i class="fa-solid fa-pencil icon"></i>
+                <h3>Colors</h3>
+              </div>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Phasellus blandit massa
+              </p>
+            </div>
+          </div>
+          <div className="accordion">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi
+              placeat deserunt veritatis, a rerum omnis reprehenderit quod in
+              debitis minus perferendis velit qui, vero temporibus nulla ipsa?
+              Labore, veniam nostrum?
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Feature;
--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Slider from '../../components/Slider/Slider'
 import Technology from '../../components/Technology/Technology'
+import Feature from '../../components/Feature/Feature'
 
 const HomePage = () => {
   return (
       <>
       <Slider />
-      <Technology/>
+      <Technology />
+      <Feature/>
       </>
   )
 }
